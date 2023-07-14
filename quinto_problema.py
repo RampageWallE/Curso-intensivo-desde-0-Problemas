634675fd5605633fdc0e65565f50bce3dd2decbd
Piero Valentino Noa Chahuayo <piero.noa@tecsup.edu.pe>
2023-07-13 21:21:06 -0500

    Quinto problema

--- quinto_problema.py
+++ quinto_problema.py
@@ -2,6 +2,6 @@ import pandas as pd
 read_xlsx = pd.read_excel("excel.xlsx")
 read_xlsx.to_csv("csv.csv", index = None, header = True)
 dataframe = pd.DataFrame(pd.read_csv("csv.csv"))
-print(dataframe)
+print(dataframe) 
 
 #Se realizo la converticion de un archivo xlsx a csv
