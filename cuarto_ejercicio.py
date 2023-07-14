383d7b2a572d9a34db00c32abcd105109113bd5b
Piero Valentino Noa Chahuayo <piero.noa@tecsup.edu.pe>
2023-07-13 20:51:50 -0500

    Cuarto ejercicio

--- cuarto_ejercicio.py
+++ cuarto_ejercicio.py
@@ -32,18 +32,4 @@ while opcion != 0:
             print("No se logro conectar a la API, ingrese un lugar valido")
     else: 
         break
-        
-
-# if response.status_code == 200:
-#     data = response.json()
-#     temp = data['main']['temp']
-#     desc = data['weather'][0]['description']
-#     print(f'Temperature: {temp} K')
-#     print(f'Description: {desc}')
-# else:
-#     print('Error fetching weather data')
-
-# respuesta = response.json()
-# pais = respuesta['sys']['country']
-
-# print(pais)
+    
